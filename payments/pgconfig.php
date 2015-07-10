112ed88e24a2b5c938b0d7945a35d5e02c4b261c
Janaka Rajapaksha <janaka@jcodes.me>
2015-07-11 00:49:38 +0530

    remove unwanted files

--- payments/pgconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $pgdomain = "www.paystage.com";
-    $pgInstanceId = "91941126";
-    $merchantId = "21402203";
-    $hashKey = "A447DDFE9A261F76";
-
-    header("pragma" . ": " . "no-cache");
-    header("cache-control" . ": " . "No-cache");
-?>
